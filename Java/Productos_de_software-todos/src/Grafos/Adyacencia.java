27581cd24a30776b7459174ef0f25f6f2f2935c0
valentinawerle <valentinawerle@gmail.com>
2023-11-22 01:24:10 -0300

    subida productos software todos para final

---
+++ Java/Productos_de_software-todos/src/Grafos/Adyacencia.java
@@ -0,0 +1,154 @@
+package Grafos;
+
+import java.util.ArrayList;
+
+public class Adyacencia {
+
+    //Atributos
+    private String [][] matriz;
+    private int tamanio;
+
+    public int getTamanio() {
+        return tamanio;
+    }
+
+    //Constructor
+    public Adyacencia (int tamanio){
+
+        matriz = new String [tamanio][tamanio];
+        this.tamanio = tamanio;
+    }
+
+
+    public void cargar (String valor, int x, int y){  //x = fila, y = columna
+
+        if(!existe(x,y)){
+            matriz[x][y] = valor;
+        }else{
+            System.out.println("Ya existe un valor en esa posicion");
+
+        }
+
+    }
+
+    public boolean existe(int x, int y){
+        if(matriz[x][y] == null){
+            return false;
+        }else {
+            return true;
+        }
+    }
+    public void imprimir() {
+        for (int i = 0; i < tamanio; i++) {
+            for (int j = 0; j < tamanio; j++) {
+                System.out.print("+-------"); // Línea horizontal de separación
+            }
+            System.out.println("+");
+
+            for (int j = 0; j < tamanio; j++) {
+                System.out.printf("| %6s", matriz[i][j]); // Valor con separador vertical y ancho mínimo de 3 caracteres
+            }
+            System.out.println("|");
+        }
+
+        for (int j = 0; j < tamanio; j++) {
+            System.out.print("+-------"); // Línea horizontal de separación en la parte inferior
+        }
+        System.out.println("+");
+    }
+
+    public void BuscadorConexiones(int nodoFila) {
+        int fila = nodoFila - 1; // Se resta 1 porque los índices de la matriz empiezan en 0
+
+        if (fila != -1) { // Verifica si el nodoFila existe en la matriz
+            ArrayList<Integer> conecciones = new ArrayList<>(); // Crea una lista para almacenar las conexiones (números de columna)
+            ArrayList<String> conectores = new ArrayList<>(); // Crea una lista para almacenar los conectores
+
+            for (int j = 0; j < tamanio; j++) { // Itera sobre las columnas de la fila dada en la matriz
+                String elemento = matriz[fila][j]; // Obtiene el elemento en la posición fila, columna
+                if (elemento != null && !elemento.startsWith("0")) { // Verifica si el elemento no es nulo y comienza con "e"
+                    conecciones.add(j + 1); // Agrega el número de columna (más 1, ya que los índices comienzan en 0)
+                    conectores.add(elemento); // Agrega el elemento (conector) a la lista de conectores
+                }
+            }
+
+            // Imprimir los elementos encontrados
+            if (!conecciones.isEmpty()) { // Verifica si se encontraron conexiones
+                System.out.println("Con quien está conectado el nodo " + nodoFila + ":\n");
+                for (int i = 0; i < conecciones.size(); i++) { // Itera sobre la lista de conexiones
+                    System.out.println("Con el nodo: " + conecciones.get(i) + " Mediante el conector: " + conectores.get(i));
+                }
+            } else {
+                System.out.println("No se encontraron conexiones con otros nodos desde el nodo " + nodoFila);
+            }
+        } else {
+            System.out.println("El nodo " + nodoFila + " no existe en la matriz.");
+        }
+    }
+
+    public void vaciar() {
+        matriz = null; // Asigna null a la matriz
+    }
+
+    public void modificar(int x, int y, String valor) {
+        if (x >= 0 && x < tamanio && y >= 0 && y < tamanio) {
+            matriz[x][y] = valor;
+            matriz[y][x] = valor;
+            imprimir();
+        } else {
+            System.out.println("Coordenadas fuera de rango. No se puede modificar la celda.");
+        }
+    }
+
+    public void eliminarNodo(int indiceNodo) {
+        if (indiceNodo >= 0 && indiceNodo < tamanio) {
+            // Eliminar la fila del nodo
+            for (int i = indiceNodo; i < tamanio - 1; i++) {
+                for (int j = 0; j < tamanio; j++) {
+                    matriz[i][j] = matriz[i + 1][j];
+                }
+            }
+
+            // Eliminar la columna del nodo
+            for (int i = 0; i < tamanio; i++) {
+                for (int j = indiceNodo; j < tamanio - 1; j++) {
+                    matriz[i][j] = matriz[i][j + 1];
+                }
+            }
+
+            // Actualizar la lista de nodos
+            tamanio--;
+            System.out.println("Nodo eliminado con exito");
+        } else {
+            System.out.println("Índice de nodo fuera de rango. No se puede eliminar el nodo.");
+        }
+        imprimir();
+
+    }
+
+    public void AgregarNodo() {
+        String [][] matrizAux = new String [tamanio + 1][tamanio + 1];
+        for (int i = 0; i < tamanio; i++) {
+            for (int j = 0; j < tamanio; j++) {
+                matrizAux[i][j] = matriz[i][j];
+            }
+        }
+        matriz = matrizAux;
+        tamanio++;
+        System.out.println("Nodo agregado con exito");
+        imprimir();
+    }
+
+
+    public boolean existeMatriz() {
+        if (matriz == null) {
+            return false;
+        } else {
+            return true;
+        }
+
+
+    }
+
+
+}
---
+++ Java/Productos_de_software-todos/src/Grafos/Main.java
@@ -0,0 +1,181 @@
+package Grafos;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int cantNodos = -1;
+        Adyacencia A1= null;
+        String opcionMenu = "0";
+
+
+        while(opcionMenu != "8"){
+
+            System.out.println("\nIngrese lo que desea hacer:\n" +
+                    "1- Cargar matriz de Adyacencia\n" +
+                    "2- imprimir matriz de Adyacencia\n" +
+                    "3- Buscar conecciones por nodo\n" +
+                    "4- Borrar la Matriz Cargada\n" +
+                    "5- Eliminar/modificar una coneccion\n" +
+                    "6- Eliminar Nodo\n" +
+                    "7- Agregar Nodo\n" +
+                    "8- salir");
+            opcionMenu = scanner.next();
+
+            switch (opcionMenu) {
+                case "1":
+                    boolean inputCantNodo = false; // Variable para verificar si la cantidad de nodos es válida
+                    boolean inputValor;
+                    while(!inputCantNodo) {
+                        System.out.println("Ingrese la cantidad de Nodos que tiene la Matriz que intenta cargar");
+                        try { // Se intenta convertir el valor ingresado a un número entero
+                            cantNodos = scanner.nextInt(); // Se ingresa el valor
+                            if (cantNodos > 0) { // Si el valor es mayor que 0
+                                inputCantNodo = true; // Se acepta la entrada si es un número entero mayor que 0
+                            } else { // Si el valor es menor o igual que 0
+                                System.out.println("Error: El número debe ser mayor que 0.");
+                            } // Se acepta la entrada si no hay excepción
+                        } catch (Exception e) { // Si el valor no es un número entero
+                            System.out.println("Error: Debe ingresar un número entero.");
+                            scanner.next(); // Limpiar el buffer del Scanner
+                        }
+
+                    }
+                    A1 = new Adyacencia(cantNodos);
+                    System.out.println("Carguemos los valores de tu tabla de Adyacencia");
+                    String valor;
+
+                    for (int x = 1; x <= cantNodos; x++) {
+                        for (int y = 1; y <= cantNodos; y++) {
+                            if (!A1.existe(x - 1, y - 1)) {
+                                System.out.println("Ingrese el Valor de  la posicion: [" + y + "]/[" + x + "]");
+                                inputValor = false;
+                                    while(!inputValor) { //Mientras no se haya ingresado un valor válido
+                                        valor = scanner.next(); //Se ingresa el valor
+                                        if (valor.length() <= 6) { //Si el valor tiene un máximo de 6 caracteres
+                                            inputValor = true;  //Se acepta el valor
+                                            if (x != y) { //Si no es la diagonal principal
+                                                A1.cargar(valor, x - 1, y - 1); //Carga el valor en la posicion [x]/[y]
+                                                A1.cargar(valor, y - 1, x - 1); //Carga el valor en la posicion [y]/[x]
+                                            } else { //Si es la diagonal principal
+                                                A1.cargar(valor, x - 1, y - 1); //Carga el valor en la posicion [x]/[y]
+                                            }
+                                        } else {
+                                            System.out.println("Error: El valor debe tener un máximo de 6 caracteres.");
+                                        }
+                                    }
+                                A1.imprimir();
+                                }
+                            }
+                        }
+
+                    break;
+                case "2":
+                    if (A1.existeMatriz()) {
+                        A1.imprimir();
+                    } else {
+                        System.out.println("La matriz no se ha inicializado.");
+                    }
+                    break;
+                case "3":
+                    if (A1.existeMatriz()) {
+                        int nodoBusqueda = -1;
+                        boolean inputNodoBusqueda = false; // Variable para verificar si el nodoBusqueda es válido
+                        while(!inputNodoBusqueda) { // Mientras no se haya ingresado un valor válido
+                            System.out.println("Ingrese el nodo del cual desea buscar sus conexiones");
+                            try { // Se intenta convertir el valor ingresado a un número entero
+                                nodoBusqueda = scanner.nextInt(); // Se ingresa el valor
+                                if (nodoBusqueda > 0 && nodoBusqueda <= cantNodos) { // Si el valor está entre 1 y la cantidad de nodos
+                                    inputNodoBusqueda = true; // Se acepta la entrada si es un número entero mayor que 0
+                                } else {
+                                    System.out.println("Error: El número debe estar en los parametros establecidoa anteriormente.");
+                                } // Se acepta la entrada si no hay excepción
+                            } catch (Exception e) {
+                                System.out.println("Error: Debe ingresar un número entero.");
+                                scanner.next(); // Limpiar el buffer del Scanner
+                            }
+                        }
+                        A1.BuscadorConexiones(nodoBusqueda);
+                    } else {
+                        System.out.println("La matriz no se ha inicializado.");
+                    }
+                    break;
+                case "4":
+                    if (A1.existeMatriz()) {
+                        A1.vaciar();
+                        System.out.println("Matriz eliminada.");
+                    } else {
+                        System.out.println("La matriz no se ha inicializado.");
+                    }
+                    break;
+                case "5":
+                    if (A1.existeMatriz()) { //Si la matriz existe
+                        int x;
+                        int y;
+                        // ingreso de coordenadas para la modificacion
+                        String modi = null;
+                        System.out.println("Ingrese las coordenadas que desea modificar\nX:");
+                        x = scanner.nextInt();
+                        System.out.println("Y:");
+                        y = scanner.nextInt();
+                        System.out.println("Por qué desea modificarlo? (si desea eliminarlo ingrese null)");
+                        modi = scanner.next();
+                        A1.modificar(y - 1, x - 1, modi);
+                    } else {
+                        System.out.println("La matriz no se ha inicializado.");
+                    }
+                    break;
+                case "6":
+                    if (A1.existeMatriz()) {
+                        int nodoAeliminar;
+                        System.out.println("Ingrese el nodo que desea eliminar:");
+                        nodoAeliminar = scanner.nextInt(); //Se ingresa el nodo a eliminar
+                        A1.eliminarNodo(nodoAeliminar - 1); // Se resta 1 porque los índices de la matriz empiezan en 0
+                    } else {
+                        System.out.println("La matriz no se ha inicializado.");
+                    }
+                    break;
+
+                case "7":
+                    if (A1.existeMatriz()) {
+
+                        A1.AgregarNodo(); //Se agrega un nodo a la matriz
+                        cantNodos = A1.getTamanio(); //Se obtiene el tamaño de la matriz
+                        for (int x = 1; x <= cantNodos; x++) {  //Se itera sobre las filas de la matriz
+                            if (!A1.existe(x - 1, cantNodos - 1)) { //Si no existe el nodo en la fila
+                                System.out.println("Ingrese el Valor de  la posicion: [" + x + "]/[" + cantNodos + "]");
+                                valor = scanner.next();
+                                if(x != cantNodos){
+                                    A1.cargar(valor, x - 1, cantNodos - 1);
+                                    A1.cargar(valor, cantNodos - 1, x - 1);
+                                }else{
+                                    A1.cargar(valor, x - 1, cantNodos - 1);
+                                }
+                                A1.imprimir();
+                            }
+                        }
+
+
+
+                    } else {
+                        System.out.println("La matriz no se ha inicializado.");
+                    }
+                    break;
+
+                case "8":
+                    System.out.println("Saludos!");
+                    break;
+
+                default:
+                    System.out.println("Ingrese un valor válido");
+                    break;
+            }
+
+
+        }
+
+    }
+}
