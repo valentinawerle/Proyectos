77dac37ec2854f13c9553dbafb9aa95c74ba8078
valentinawerle <valentinawerle@gmail.com>
2023-11-23 14:41:45 -0300

    subida clases genericas

---
+++ Java/ClasesGenericas/src/genericos/ClaseGenerica.java
@@ -0,0 +1,15 @@
+package genericos;
+
+public class ClaseGenerica <T> {
+
+    T objeto;
+
+    public ClaseGenerica(T objeto) {
+        this.objeto = objeto;
+    }
+
+    public void obtenerTipo(){
+        System.out.println("El tipo T es: " + objeto.getClass().getSimpleName()); // .getSimpleName() devuelve el nombre de la clase
+    }
+
+}
