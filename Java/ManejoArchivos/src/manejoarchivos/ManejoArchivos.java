be09105566648750da157e25576131db9b343bdc
valentinawerle <valentinawerle@gmail.com>
2023-11-25 00:25:08 -0300

    subida manejo archivos

--- Java/ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
+++ Java/ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
@@ -1,4 +1,22 @@
 package manejoarchivos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class ManejoArchivos {
+
+    public static void crearArchivo(String nombreArchivo) {
+        File archivo = new File(nombreArchivo);
+        try {
+            PrintWriter salida = new PrintWriter(archivo);
+            salida.close();
+            System.out.println("El archivo se ha creado correctamente");
+        } catch (FileNotFoundException ex) {
+           ex.printStackTrace(System.out);
+        }
+
+
+    }
+
 }
