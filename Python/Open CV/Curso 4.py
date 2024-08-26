aaa3181295e79dfcd4ac97753b73ba8e8d349b38
Valentina Werle <147965244+valentinawerle@users.noreply.github.com>
2024-08-26 17:41:56 -0300

    dsdd

---
+++ Python/Open CV/Curso 4.py
@@ -0,0 +1,28 @@
+#Manipulacion de imagenes
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+imagen = cv2.imread("/home/valentinawerle/Documentos/Imagenes/perrito.jpg")
+
+if imagen is not None:
+    print(f"Tipo de la imagen: {type(imagen)}")
+    print(f"Dimensiones de la imagen: {imagen.shape}")
+
+    imagen2 = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
+    imagen2_rgb = cv2.resize(imagen2, (300, 300))
+
+    # Muestra la imagen usando matplotlib
+    plt.imshow(imagen2_rgb)
+    plt.title("Perrito")
+    plt.axis('off')  # Oculta los ejes
+    plt.show()
+
+
+else:
+    print(f"No se pude cargar la imagen")
+
+
+
+
