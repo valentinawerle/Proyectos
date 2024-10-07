896c49690f914c0ce54ca6261195ebd9df796e96
Valentina Werle <valentinawerle@gmail.com>
2024-10-07 10:27:56 -0300

    curso nest

--- Nest/01-TypeScript-intro/src/main.ts
+++ Nest/01-TypeScript-intro/src/main.ts
@@ -1,3 +1,4 @@
+import {name} from './bases/01-types.ts'
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
@@ -11,7 +12,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>mi nombre es ${name}</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
