d1268bb3f8e6352c71bd49e386e2a596bf8af297
Valentina Werle <valentinawerle@gmail.com>
2024-10-19 11:45:25 -0300

    logos

--- Desarrollo web/WebPersonal/main.js
+++ Desarrollo web/WebPersonal/main.js
@@ -1,24 +1,24 @@
 // Stack data
 const languages = [
-    { name: 'Python', icon: '/python-logo.png' },
-    { name: 'Java', icon: '/java-logo.png' },
-    { name: 'C', icon: '/c-logo.png' },
-    { name: 'TypeScript', icon: '/typescript-logo.png' },
-    { name: 'HTML', icon: '/html-logo.png' },
-    { name: 'CSS', icon: '/css-logo.png' },
-    { name: 'JavaScript', icon: '/javascript-logo.png' },
-    { name: 'Arduino', icon: '/arduino-logo.png' },
+    { name: 'Python', icon: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg' },
+    { name: 'Java', icon: 'https://img.icons8.com/?size=100&id=13679&format=png&color=000000' },
+    { name: 'C', icon: 'https://img.icons8.com/?size=512&id=40670&format=png' },
+    { name: 'TypeScript', icon: 'https://img.icons8.com/?size=100&id=wpZmKzk11AzJ&format=png&color=000000' },
+    { name: 'JavaScript', icon: 'https://img.icons8.com/?size=100&id=108784&format=png&color=000000' },
+    { name: 'HTML', icon: 'https://img.icons8.com/?size=100&id=20909&format=png&color=000000' },
+    { name: 'CSS', icon: 'https://img.icons8.com/?size=100&id=21278&format=png&color=000000' },
+    { name: 'Arduino', icon: 'https://img.icons8.com/?size=100&id=13444&format=png&color=000000' },
 ];
 
 const tools = [
-    { name: 'Jira', icon: '/jira-logo.png' },
-    { name: 'Linux', icon: '/linux-logo.png' },
-    { name: 'Figma', icon: '/figma-logo.png' },
-    { name: 'Xcode', icon: '/xcode-logo.png' },
-    { name: 'GitHub', icon: '/github-logo.png' },
-    { name: 'VS Code', icon: '/vscode-logo.png' },
-    { name: 'Notion', icon: '/notion-logo.png' },
-    { name: 'SQL', icon: '/sql-logo.png' },
+    { name: 'Jira', icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968875.png' },
+    { name: 'Linux', icon: 'https://img.icons8.com/?size=100&id=17842&format=png&color=000000' },
+    { name: 'Figma', icon: 'https://img.icons8.com/?size=100&id=W0YEwBDDfTeu&format=png&color=000000' },
+    { name: 'Postman', icon: 'https://cdn.iconscout.com/icon/free/png-256/free-cartero-3628992-3030217.png?f=webp' },
+    { name: 'GitHub', icon: 'https://img.icons8.com/m_sharp/200/FFFFFF/github.png' },
+    { name: 'VS Code', icon: 'https://img.icons8.com/?size=100&id=9OGIyU8hrxW5&format=png&color=000000' },
+    { name: 'Notion', icon: 'https://img.icons8.com/?size=100&id=nvtEH6DpqruC&format=png&color=000000' },
+    { name: 'SQL', icon: 'https://img.icons8.com/?size=100&id=qGUfLiYi1bRN&format=png&color=000000' },
 ];
 
 // Function to create stack items
