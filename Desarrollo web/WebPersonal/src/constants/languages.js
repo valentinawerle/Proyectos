8c32b8653b895e64d6024513cd654a7c3c388e33
miniscandal <oscar01dev@gmail.com>
2024-10-27 06:48:10 -0600

    refactor: reorganize css files for better local path and purpose clarity

--- Desarrollo web/WebPersonal/src/constants/languages.js
+++ Desarrollo web/WebPersonal/src/constants/languages.js
@@ -1,12 +1,12 @@
 const languages = [
-    { name: 'Python', icon: './public/icons/languages/python.svg' },
-    { name: 'Java', icon: './public/icons/languages/java.png' },
-    { name: 'TypeScript', icon: './public/icons/languages/type-script.png' },
-    { name: 'JavaScript', icon: './public/icons/languages/java-script.png' },
-    { name: 'HTML', icon: './public/icons/languages/html.png' },
-    { name: 'CSS', icon: './public/icons/languages/css.png' },
-    { name: 'Arduino', icon: './public/icons/languages/arduino.png' },
-    { name: 'C', icon: './public/icons/languages/c.png' },
+    { name: 'Python', icon: './src/assets/icons/languages/python.svg' },
+    { name: 'Java', icon: './src/assets/icons/languages/java.png' },
+    { name: 'TypeScript', icon: './src/assets/icons/languages/type-script.png' },
+    { name: 'JavaScript', icon: './src/assets/icons/languages/java-script.png' },
+    { name: 'HTML', icon: './src/assets/icons/languages/html.png' },
+    { name: 'CSS', icon: './src/assets/icons/languages/css.png' },
+    { name: 'Arduino', icon: './src/assets/icons/languages/arduino.png' },
+    { name: 'C', icon: './src/assets/icons/languages/c.png' },
 ];
 
 export { languages };
--- Desarrollo web/WebPersonal/src/constants/tools.js
+++ Desarrollo web/WebPersonal/src/constants/tools.js
@@ -1,12 +1,12 @@
 const tools = [
-    { name: 'Jira', icon: './public/icons/tools/jira.png' },
-    { name: 'Linux', icon: './public/icons/tools/linux.png' },
-    { name: 'Figma', icon: './public/icons/tools/figma.png' },
-    { name: 'Postman', icon: './public/icons/tools/postman.webp' },
-    { name: 'VS Code', icon: './public/icons/tools/vscode.png' },
-    { name: 'GitHub', icon: './public/icons/tools/github.png' },
-    { name: 'SQL', icon: './public/icons/tools/sql.png' },
-    { name: 'Notion', icon: './public/icons/tools/notion.png' },
+    { name: 'Jira', icon: './src/assets/icons/tools/jira.png' },
+    { name: 'Linux', icon: './src/assets/icons/tools/linux.png' },
+    { name: 'Figma', icon: './src/assets/icons/tools/figma.png' },
+    { name: 'Postman', icon: './src/assets/icons/tools/postman.webp' },
+    { name: 'VS Code', icon: './src/assets/icons/tools/vscode.png' },
+    { name: 'GitHub', icon: './src/assets/icons/tools/github.png' },
+    { name: 'SQL', icon: './src/assets/icons/tools/sql.png' },
+    { name: 'Notion', icon: './src/assets/icons/tools/notion.png' },
 ];
 
 export { tools };
