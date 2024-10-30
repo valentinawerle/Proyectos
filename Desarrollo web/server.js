fb6cd5c51edef0b48fd06230eb073f711a11453b
Valentina Werle <147965244+valentinawerle@users.noreply.github.com>
2024-10-30 15:58:32 -0300

    backend

---
+++ Desarrollo web/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/submit-form', async (req, res) => {
+    const formData = req.body;
+
+    try {
+        // envia los datos al API de salesforce
+        const response = await axios.post('https://webto.salesforce.com/servlet/servlet.WebToCase?encoding=UTF-8&orgId=00DHs000000QSJA', {
+            ...formData,
+            orgId: '00DHs000000QSJA',
+            retURL: 'https://www.lepton.com.ar/web/soporte/soporte-tecnico'
+        });
+
+        // redirecciona o envia una respuesta al cliente
+        res.redirect('https://www.lepton.com.ar/web/soporte/soporte-tecnico');
+    } catch (error) {
+        console.error('Error al enviar el formulario:', error);
+        res.status(500).send('Hubo un problema al enviar el formulario.');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
